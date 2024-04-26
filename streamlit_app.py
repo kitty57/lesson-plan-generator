dcad49ecc2e69865b3a93b1b59e26b0b454b5fb4
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-04-26 14:58:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,27 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title('🎈 App Name')
+genai.configure(api_key='AIzaSyDlBFVsmV8pao6Ax-bcR0dc5h4CusiNCsc')  
+model = genai.GenerativeModel(model_name="gemini-pro")
 
-st.write('Hello world!')
+def prompt(subject, topic, duration):
+    prompt_parts = [
+        f"'As an experienced teacher and professor, who loves teaching Craft lesson plans with learning objectives, engaging activities, and assessment strategies for the topic:{topic} in subject:{subject} to teach your students with for the duration:{duration}.'",
+    ]
+    return prompt_parts
+
+def generate_info_about(topic, subject, duration, prompt=prompt, model=model):
+    human_prompt = prompt(subject, topic, duration)
+    response = model.generate_content(human_prompt)
+    return response.text
+
+st.title("Craft Lesson Plan Generator")
+
+subject = st.text_input("Subject:")
+topic = st.text_input("Topic:")
+duration = st.text_input("Duration:")
+
+if st.button("Generate Lesson Plan"):
+    with st.spinner('Generating...'):
+        lesson_plan = generate_info_about(topic, subject, duration)
+        st.success(lesson_plan)
